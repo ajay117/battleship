484ba63ee4000104c6895c37474d82e6641182ab
Ajay KC <meajay64@gmail.com>
2022-10-02 12:41:45 +0900

    Change parameters

--- factories/Gameboard.js
+++ factories/Gameboard.js
@@ -1,18 +1,20 @@
 import Ship from './Ship'
+import { shipTypes } from '../modules/shipTypes'
 
 export default function GameBoard(length) {
   let gameBoard = new Array(length).fill(null)
-  let placeShip = (location, carrier) => {
+  let carrier = Ship(shipTypes.carrier)
+  let placeShip = (locationCarrier) => {
     let boardEnd = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
-    if (boardEnd.some((elem) => elem < location + carrier.shipArr.length - 1)) {
+    if (boardEnd.some((elem) => elem < locationCarrier + carrier.shipArr.length - 1)) {
       return 'Cannot place ship'
     } else {
       let board = [...gameBoard]
-      board.splice(location, carrier.shipArr.length)
+      board.splice(locationCarrier, carrier.shipArr.length)
       carrier.shipArr.forEach((element) => {
         let obj = { hasShip: true, isHit: false }
-        board.splice(location, 0, obj)
-        location++
+        board.splice(locationCarrier, 0, obj)
+        locationCarrier++
       })
       return board
     }
